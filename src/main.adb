0f2769310f3c996b89b1347731d78b29999bf8bf
AndyT97 <andy.ta991@gmail.com>
2020-11-03 17:42:27 -0500

    Redeclared functions above evaluateSeries procedure

--- src/main.adb
+++ src/main.adb
@@ -10,6 +10,22 @@ procedure Main is
    --declare variables here
    input : Integer;
 
+   function getExactError(exact:Float; approx:Float) return Float is
+   begin
+      return (100.0 * (exact - approx) / exact);
+   end getExactError;
+
+   function getTruncError(first:Float; approx:Float) return Float is
+   begin
+      return (100.0 * first / approx);
+   end getTruncError;
+
+   procedure printThis(interval:Float; approx:Float; exact:Float; exactError:Float; relativeError:Float) is
+   begin
+      Put_Line (Float'Image (interval));
+     -- Ada.Float_Text_IO.Put(interval,1,6,4);Put("   ");Put(approx,1,6,4);Put("   ");Put(exact,1,6,4);Put("   ");Put(exactError,1,6,4);Put("   ");Put(relativeError,1,6,4);
+   end printThis;
+
    procedure evaluateSeries is
       -- TODO change termCount to int maybe
    termCount, rangeInput, xVal, exact,
@@ -45,39 +61,31 @@ procedure Main is
             approx := 1.0 - xVal;
             trunc := (xVal * xVal * xVal) / 3.0;
          elsif termCount = 3.0 then
-            approx := 1 - xVal + (xVal * xVal * xVal) / 3.0;
+            approx := 1.0 - xVal + (xVal * xVal * xVal) / 3.0;
             trunc := (xVal * xVal * xVal * xVal) / 6.0;
          elsif termCount = 4.0 then
-            approx := 1 - xVal + (xVal * xVal * xVal) / 3.0 - (xVal * xVal * xVal * xVal) / 6.0;
+            approx := 1.0 - xVal + (xVal * xVal * xVal) / 3.0 - (xVal * xVal * xVal * xVal) / 6.0;
             trunc := (xVal * xVal * xVal * xVal * xVal) / 30.0;
          elsif termCount = 5.0 then
-            approx := 1 - xVal + (xVal * xVal * xVal) / 3.0 - (xVal * xVal * xVal * xVal) / 6.0 +
+            approx := 1.0 - xVal + (xVal * xVal * xVal) / 3.0 - (xVal * xVal * xVal * xVal) / 6.0 +
             (xVal * xVal * xVal * xVal * xVal) / 30.0;
             trunc := (xVal * xVal * xVal * xVal * xVal * xVal * xVal) / 630.0;
          end if;
+
+         exactError := getExactError(exact,approx);
+         truncError := getTruncError(trunc,approx);
+         printThis(xVal, approx, exact, exactError, truncError);
+
 
       end loop;
 
 
    end evaluateSeries;
 
-   function getExactError(exact:Float; approx:Float) return Float is
-   begin
-      return (100.0 * (exact - approx) / exact);
-   end getExactError;
-
-   function getTruncError(first:Float; approx:Float) return Float is
-   begin
-      return (100.0 * first / approx);
-   end getTruncError;
-
-   procedure printThis is
-   begin
-      Put_Line(" ");
-   end printThis;
 
 
 begin
+   -- Main menu
    loop
 
       Put_Line("Evaluate the Maclaurin Series approximation to exp"); -- Put_Line is like .println of java
